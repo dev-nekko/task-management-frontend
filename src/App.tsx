06772615c8fc6e7712fdc6785bc55b686b012d00
xfrogz <renattoyague@gmail.com>
2022-05-17 00:36:25 -0500

    Add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import { TaskAPI } from './api/task.api';
 import { TaskDTO } from './api/dto/task.dto';
-
+import { Grid } from '@mui/material';
 
 function App() {
 
@@ -22,14 +22,17 @@ function App() {
 
   return (
     <div className="App">
-      <ul>
-          {
-        tasks.map(task => {
-          return <li>{task.title}</li>
-        })
-      }
-      </ul>
-    
+      <Grid container spacing={1}>
+        {tasks.map(task => {
+          return(
+            <Grid item xs={3}>
+              Title: {task.title}
+            <br/>
+              Description: {task.description}
+            </Grid>
+          );
+        })}
+      </Grid>
     </div>
   );
 }
